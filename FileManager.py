ad3a095613ca8eea4d126cb87e1be468e097b302
Ahm69dms <ahmadou.dieng1@ecoles-epsi.net>
2024-10-23 12:35:57 +0200

    update Filemanager and main

--- FileManager.py
+++ FileManager.py
@@ -2,7 +2,16 @@ class FileManager:
     #initialisation de la classe FileManager avec le chemin du fichier
     def __init__(self, file_path):
         self.file_path = file_path # chemin du fichier
-        self.file_nombres_de_lignes = self.get_count()
         
-    # classe pour retourner le nom du fichier
+    def read_file(self):
+        pass
+    
+    def write_to_file(self,data):
+        pass
+    
+    def count_lines(self):
+        pass
+
+    def search_keyword(self,keyword):
+        pass    
    
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 import FileManager
+
+if __name__ == "__main__":
+    gestion_fichier=FileManager()
