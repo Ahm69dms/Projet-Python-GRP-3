ab3439461a5ca09c360ba7f6a501b3c3e6e2ba11
Habib_Meka <baanameka@gmail.com>
2024-10-23 12:09:32 +0200

    update file

--- FileManager.py
+++ FileManager.py
@@ -1,2 +1,8 @@
-print ("ceci est un wordpress")
-b = input("veuillez entrer un nom: ")
+class FileManager: 
+    #initialisation de la classe FileManager avec le chemin du fichier
+    def __init__(self, file_path):
+        self.file_path = file_path # chemin du fichier
+        self.file_nombres_de_lignes = self.get_count()
+        
+    # classe pour retourner le nom du fichier
+   
