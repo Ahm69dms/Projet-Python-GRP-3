22db5f377e0b6d591cd5333bacfefbc4e0e6b3ee
Aichatou270 <adeossinanaaichath@gmail.com>
2024-10-24 12:58:19 +0200

    Update FileManager.py

--- FileManager.py
+++ FileManager.py
@@ -4,14 +4,29 @@ class FileManager:
         self.file_path = file_path # chemin du fichier
         
     def read_file(self):
-        pass
+        "Lire et affiche le contenu du fichier."
+        try:
+            with open(self.file_path, 'r') as fie:
+                contenu = file.read()
+            print(contenu)
+        except FileNotFoundError:
+            print(f"Erreur : Le fichier '{sel.file_path}' n'existe pas, il est crée.
+            
     
     def write_to_file(self,data):
-        pass
+         "Ecrire des données dans le fichier."
+        try:
+            with open(sel.file_path, 'a') as file:    
+            
+                file.write(data + '\n')
+            print(f"Données ajoutées au fichier '{self.file_path}'.")
+        except Exception as e:
+            print(f'Erreur lors de l'ecriture dans le fichier : {e}")
+                
     
     def count_lines(self):
-        pass
+         "Retoune le nombre de lignes dans le fichier."
 
     def search_keyword(self,keyword):
-        pass    
+         "Recherche un mot clé dans le fichier et affiche les lignes correspondantes."   
    
