efc42feeed108ee872a8dca4855bb185ff486f69
Habib_Meka <baanameka@gmail.com>
2024-10-24 13:48:38 +0200

    mise à jour du code

--- FileManager.py
+++ FileManager.py
@@ -3,15 +3,62 @@ class FileManager:
     def __init__(self, file_path):
         self.file_path = file_path # chemin du fichier
         
+    """la fonction read_file() Lit et affiche le contenu du fichier. si il est absent le code renvoi un message"""    
     def read_file(self):
-        pass
-    
-    def write_to_file(self,data):
-        pass
-    
+        try:
+            with open(self.file_path, 'r') as mon_fichier:
+                contenu_fichier = mon_fichier.read()
+                print(contenu_fichier)
+        except FileNotFoundError: 
+            print("----ERROR, FILE NOT FOUND----")
+        
+    """la fonction pour écrire les données dans le fichier."""  
+    def write_to_file(self, data):
+        data = input("Entrez les données à ajouter au fichier: ")
+        try:
+            with open(self.file_path, 'a') as file: # ouvrir le fichier en mode append, sans supprimer le contenu précedent
+                file.write(data + '\n')  # Écrire les données dans le fichier
+                print(f"Les données ont été écrites avec succès dans {self.file_path}.")
+        except Exception as erreur:  # Capture toute erreur
+            print(f"Erreur lors de l'écriture dans le fichier : {erreur}")  # Affiche le message d'erreur
+        
+    """la fonction qui retourne le nombre de lignes dans le fichier."""
     def count_lines(self):
-        pass
+        try:
+            with open(self.file_path, 'r') as mon_fichier:
+                nombres_lignes = mon_fichier.readlines()
+                return len(nombres_lignes)
+        except FileNotFoundError:
+            return 0
 
-    def search_keyword(self,keyword):
-        pass    
+    """Recherche un mot-clé dans le fichier et affiche les lignes correspondantes."""
+    def search_keyword(self, keyword):
+        
+    #"""Recherche un mot-clé dans le fichier et affiche les lignes correspondantes, tout en comptant le nombre d'occurrences."""
+    
+        keyword = input("Entrez le mot-clé : ").lower()  # Gestion de la casse
+
+        total = 0
+        try:
+            with open(self.file_path, 'r') as mon_fichier:  # Ouvre en mode lecture seule
+                trouver = False
+            
+                for numero_ligne, line in enumerate(mon_fichier, start=1):
+                    if keyword in line.lower():  # Comparer la ligne et le mot-clé en minuscules
+                        print(f"Mot-clé trouvé à la ligne {numero_ligne}: {line.strip()}")
+                        trouver = True
+                        total += line.lower().count(keyword)  # Compter les occurrences du mot-clé dans la ligne
+            
+            print(f"Le mot '{keyword}' apparaît {total} fois dans le fichier.")
+            
+            if not trouver:
+                    print(f"Le mot-clé '{keyword}' n'a pas été trouvé dans le fichier.")
+        except FileNotFoundError:
+                print("---ERROR, FILE NOT FOUND--")
+
+            
+        
+# Test de la classe
+print("<--DEBUT-->")
+
    
--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-import FileManager
+from FileManager import FileManager
+#if __name__ == "__main__":
 
-if __name__ == "__main__":
-    gestion_fichier=FileManager()
+"""cette commande permet de lier le fichier log.txt à l'instance de la classe FileManager, afin de pouvoir
+                  manipuler ce fichier à l'aide des méthodes définies dans la classe."""
+file_manager = FileManager("C:\\Users\\BAANA_MEKA\Desktop\\log.txt")  ## chemin vers mon fichier
+
+
+
+# Lire le contenu du fichier
+file_manager.read_file()
+
+# Écrire des données dans le fichier
+file_manager.write_to_file()
+
+# Compter les lignes et afficher le résultat
+nombre_de_lignes = file_manager.count_lines()
+print(f"Nombre de lignes dans le fichier : {nombre_de_lignes}")
+
+# Rechercher un mot-clé
+file_manager.search_keyword(keyword="mot-clé")
+print("<--FIN-->")
+print()
